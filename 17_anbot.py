b81244162888b4026e8c490cfa9b8ce31c92cf09
Juxlos <juxlos381@gmail.com>
2017-12-10 10:49:00 +0000

    expanded on the combat and distance part - skeleton

--- 17_anbot.py
+++ 17_anbot.py
@@ -9,8 +9,10 @@
 import random
 import math
 import json
+import time
 import matplotlib.pyplot as plt
 import discord
+import asyncio
 import configid
 
 
@@ -38,6 +40,22 @@ def is_number(s):
         return False
 
 
+#   To be invoked whenever the variable users need to be updated semi-permanently
+#   e.g. completing missions, getting killed, buying things, etc.
+def write_user_data():
+    with open("users.json", "w") as u:
+        json.dump(users, u, indent=4, separators=(', ',': '))
+
+
+def weightedpick(dictionary):
+    r = random.uniform(0, sum(dictionary.values()))
+    s = 0.0
+    for k, w in dictionary.items():
+        s += w
+        if r < s: return k
+    return k
+
+
 def damage(weapon, target, distance):
     #   Fairly accurate as long as distance below range
     #   After that drops off
@@ -62,38 +80,81 @@ def get_word(weapon):
     else:
         verb = ["Menyerang", "serang", "Serangan"]
     return verb
-        
-async def attack(message, target, weapon):
-    verb = get_word(weapon)
-    await bot.send_message(message.channel, "{1} dengan senjata {0}...".format(weapon, verb[0]))
-    weapon = weapons_base[weapon]
-    a = damage(weapon, 'a', weapon['range'])
-    print(a)
-    if len(a) == 0:
-        msg = "Peluru anda habis!"
-    elif max(a) == 0:
-        msg = "{0} anda meleset!".format(verb[2])
-    else:
-        msg = "{1} kena {0} kali!".format(len(a), verb[2])
-    await bot.send_message(message.channel, msg)
-    return a
-    
+
+
+async def combat_phase(message, stage):
+    #   Stage stats
+    ststats = rpg_stats['stages'][stage]
+    target = weightedpick(ststats["enemies"])
+    #   Random distance between proximity_max and 2 times that - most likely 1.5 times
+    distance = int(random.triangular(ststats["proximity_max"], 2*ststats["proximity_max"]))
+    await bot.send_message(message.channel, "Terlihat di jarak {0} meter seorang {1}...".format(str(distance), target))
+
+
+#   Saves typing it multiple times
+def quit_battle(playerid):
+    users[playerid]["in_battle"] = False
+
+
+async def battle(message, time_limit = 150, stage = "default"):
+    name = message.author.name
+    #   To prevent patrolling... message spam
+    already_sent_patrol = False
+    player_id = message.author.id
+    player = users[message.author.id]
+    await bot.send_message(message.channel, '{0} mulai bergerak...'.format(name))
+    while time_limit > 0:
+        if random.random() < rpg_stats["stages"][stage]["density"]:
+            #   10 minutes allocated to encounter and combat
+            time_limit -= 10
+            already_sent_patrol = False
+            await bot.send_message(message.channel, "{0} bertemu musuh!".format(name))
+            #   Ambush roll - player shoots first because they're the player but this is the chance to attack undetected
+            await combat_phase(message, stage)
+        else:
+            time_limit -= 6
+            if already_sent_patrol is False:
+                await bot.send_message(message.channel, "{0} terus berpatroli...".format(name))
+                already_sent_patrol = True
+        await asyncio.sleep(0.3)
+    await asyncio.sleep(0.5)
+    await bot.send_message(message.channel, 'Patroli {0} selesai!'.format(name))
+    quit_battle(player_id)
+
 
 @bot.event
 async def on_message(message):
     if message.author.id != bot.user.id and message.channel.id in ['339706179213328385', '186588826234257408']:
         #   Synonyms
         if message.content in [';battle', ';enterbattle', ';enter_battle', ';fight', ';war']:
-            if message.author.id in list(users.keys()):
-                pass
-            else:
-                await bot.send_message(message.channel, '{0}, tolong daftar dulu')
-
+            if message.author.id not in list(users.keys()):
+                users[message.author.id] = {
+                    "in_battle": True,
+                    "weapons":  {
+                        "RIFLE": "Arisaka 38",
+                        "PISTOL": "Nambu Type 14",
+                        "MELEE": "Bambu Runcing"
+                    },
+                    #   [Current, Max]
+                    "HP":   [100, 100],
+                    "magazines":    {
+                        "PISTOL": 5,
+                        "MELEE": 9999999999999
+                    },
+                    "last_patrol": 0
+                }
+                await bot.send_message(message.channel, '{0} telah menerima senjata.'.format(message.author.name))
+            await battle(message)
+            users[message.author.id]['last_patrol'] = time.time()
+            write_user_data()
 
 @bot.event
 async def on_ready():
+    #   Ensures all in_battle is False
+    for u in list(users.keys()):
+        users[u]["in_battle"] = False
+    write_user_data()
     print(bot.user.name)
     print(bot.user.id)
-    
 
 bot.run(configid.old_token)
